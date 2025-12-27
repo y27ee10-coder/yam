337c029479db43f9987c6ec01eb39c664f454fda
y27ee10-coder <y27ee10@gmail.com>
2025-12-27 16:46:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 50; i++) {
+  let star = document.createElement("div");
+  star.className = "star";
+  star.style.left = Math.random() * 100 + "vw";
+  star.style.top = Math.random() * 100 + "vh";
+  star.style.animationDuration = (Math.random() * 3 + 2) + "s";
+  document.body.appendChild(star);
+}
